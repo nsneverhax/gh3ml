93168af15798a4a6b463154e85079609d44c3ec8
Vultumast <152120810+Vultumast@users.noreply.github.com>
2025-01-14 10:11:40 -0500

    Update main.cpp
    restore debug logging to gh3

--- src/gh3ml/main.cpp
+++ src/gh3ml/main.cpp
@@ -1,7 +1,11 @@
 #include <Windows.h>
+#include <intrin.h>
+
+#include <iostream>
 
 #include <GH3ML.hpp>
 #include <Log.hpp>
+#include <MinHook.h>
 
 HANDLE _gh3Handle = nullptr;
 
@@ -10,6 +14,27 @@ const HANDLE gh3ml::GetGH3Handle()
     return _gh3Handle;
 }
 
+
+
+typedef void(__stdcall* func_Debug_Log)(char* fmt, ...);
+
+func_Debug_Log Debug_Log = nullptr;
+func_Debug_Log Debug_Log_Target = reinterpret_cast<func_Debug_Log>(0x00472b50);
+
+
+void __stdcall detourDebugLog(char* fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+
+    std::cout << "GH3 : " << fmt << std::endl;
+
+    Debug_Log(fmt, args);
+    va_end(args);
+}
+
+
+HMODULE gh3Module = nullptr;
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
@@ -20,9 +45,23 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
     case DLL_PROCESS_ATTACH:
         DisableThreadLibraryCalls(hinstDLL);
 
+        gh3ml::Log::CreateConsole();
+
+        if (MH_Initialize() != MH_OK)
+        {
+            // TODO: this
+        }
+
+        if (MH_CreateHook(reinterpret_cast<void**>(Debug_Log_Target), &detourDebugLog, reinterpret_cast<void**>(&Debug_Log)) != MH_OK)
+        {
+            std::cout << "Unable to get Hook!" << std::endl;
+        }
+        std::cout << "Hooked!" << std::endl;
+
+        MH_EnableHook(reinterpret_cast<void**>(Debug_Log_Target));
+
         _gh3Handle = GetCurrentProcess();
 
-        gh3ml::Log::CreateConsole();
 
         break;
 
