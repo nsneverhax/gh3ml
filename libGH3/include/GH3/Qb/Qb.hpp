79ec151c4b420b6d9f01931932e812e1da8c6bcc
Vultumast <152120810+Vultumast@users.noreply.github.com>
2025-01-16 05:32:50 -0500

    compile error

--- libGH3/include/GH3/Qb/Qb.hpp
+++ libGH3/include/GH3/Qb/Qb.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 
+#include <GH3/Keys.hpp>
 #include <GH3/Qb/QbNodeFlags.hpp>
 #include <GH3/Qb/QbStructItem.hpp>
 #include <GH3/Qb/QbStruct.hpp>
@@ -10,13 +11,17 @@
 namespace GH3::Functions
 {
 
-	typedef QbStruct* (__cdecl* func_qbMalloc)(size_t size, std::uint32_t unknown);
-	typedef void* (__cdecl* func_qbArrMalloc)(size_t size, std::uint32_t unknown);
-	typedef QbStructItem* (__cdecl* func_qbItemMalloc)(size_t size, std::uint32_t unknown);
-	typedef void* (__cdecl* func_qbFree)(void* qbStruct);
+	typedef QbStruct* (*func_qbMalloc)(size_t size, std::uint32_t unknown);
+	typedef void* (*func_qbArrMalloc)(size_t size, std::uint32_t unknown);
+	typedef QbStructItem* (*func_qbItemMalloc)(size_t size, std::uint32_t unknown);
+	typedef void* (*func_qbFree)(void* qbStruct);
 
-	constexpr func_qbMalloc qbMalloc = reinterpret_cast<func_qbItemMalloc>(0x00402370);
-	constexpr func_qbArrMalloc qbArrMalloc = reinterpret_cast<func_qbItemMalloc>(0x004373A0);
-	constexpr func_qbItemMalloc qbItemMalloc = reinterpret_cast<func_qbItemMalloc>(0x00437410);
-	constexpr func_qbFree qbFree = reinterpret_cast<func_qbFree>(0x004023E0);
+	static func_qbMalloc qbMalloc = reinterpret_cast<func_qbMalloc>(0x00402370);
+	static func_qbArrMalloc qbArrMalloc = reinterpret_cast<func_qbArrMalloc>(0x004373A0);
+	static func_qbItemMalloc qbItemMalloc = reinterpret_cast<func_qbItemMalloc>(0x00437410);
+	static func_qbFree qbFree = reinterpret_cast<func_qbFree>(0x004023E0);
+
+
+
+	typedef void(__thiscall InsertCStringItem)(QbStruct* _this, Key key, char* string);
 }
--- libGH3/src/main.cpp
+++ libGH3/src/main.cpp
@@ -1,2 +1,2 @@
 #include <GH3/Keys.hpp>
-#include <GH3/Qb/QbStruct.hpp>
+#include <GH3/Qb/Qb.hpp>
